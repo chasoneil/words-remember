79da4bfab91ce30a28710cc23043a7acd733b614
chason <chason001@126.com>
2023-12-12 17:40:22 +0800

    fix upload if write is '-'

--- src/main/java/com/chason/words/service/jp/JpWordsServiceImpl.java
+++ src/main/java/com/chason/words/service/jp/JpWordsServiceImpl.java
@@ -3,6 +3,7 @@ package com.chason.words.service.jp;
 import com.chason.words.entity.jp.JpWords;
 import com.chason.words.mapper.JpWordsMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.chason.words.utils.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -48,6 +49,10 @@ public class JpWordsServiceImpl extends ServiceImpl<JpWordsMapper, JpWords> impl
             int classId = -1;
             while ((line = reader.readLine()) != null) {
 
+                if (StringUtil.isEmpty(line)) {
+                    continue;
+                }
+
                 if (line.startsWith("*")) {
                     classId = Integer.parseInt(line.replace("*", ""));
                     continue;
@@ -61,7 +66,11 @@ public class JpWordsServiceImpl extends ServiceImpl<JpWordsMapper, JpWords> impl
                 }
                 words.setWMean(split[0]);
                 words.setWRead(split[2]);
-                words.setWWrite(split[1]);
+                if (split[1].equalsIgnoreCase("-")) {
+                    words.setWWrite(split[2]);
+                } else {
+                    words.setWWrite(split[1]);
+                }
                 words.setIsRem(0);
                 this.save(words);
             }
